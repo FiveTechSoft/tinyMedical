6df8b59250f6c0be8019131300bb8320bd81d66c
FiveTech Software <antonio.fivetech@gmail.com>
2024-01-23 18:14:02 +0100

    Add files via upload

--- go16.sh
+++ go16.sh
@@ -1,2 +1,4 @@
+python3 train.py
+python3 merge.py
 python3 ../llama.cpp/convert.py --outfile tinymedical.gguf Model
 ../llama.cpp/main -ngl 32 -m tinymedical.gguf --color -c 4096 --temp 0.7 --repeat_penalty 1.1 -n -1 -i -ins
